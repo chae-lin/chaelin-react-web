1878052773a9f33c334a4f775a0967cec69fdda5
chaelin kim <gjf0055@gmail.com>
2022-03-01 20:20:22 +0900

    feat: 페이지 이동 설정

--- src/Page/Page1/index.tsx
+++ src/Page/Page1/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Page1 = () => {
   return (
     <div>
       <div>페이지1</div>
+      <Link to="/page2">Page 2</Link>
     </div>
   );
 };
--- src/Page/Page2/index.tsx
+++ src/Page/Page2/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Page2 = () => {
   return (
     <div>
       <div>페이지2</div>
+      <Link to="/">Page 1</Link>
     </div>
   );
 };
