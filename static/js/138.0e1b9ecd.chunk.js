1ccbc22d408bfee9794b3d8dd21aa77db0a617c2
chaelin kim <gjf0055@gmail.com>
2022-03-16 20:42:26 +0900

    Updates

---
+++ static/js/138.0e1b9ecd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkchaelin_react_web=self.webpackChunkchaelin_react_web||[]).push([[138],{2138:function(e,c,n){n.r(c);n(2791);var r=n(3504),a=n(184);c.default=function(){return(0,a.jsxs)("div",{children:[(0,a.jsx)("div",{children:"\ud398\uc774\uc9c01"}),(0,a.jsx)(r.rU,{to:"/page2",children:"Page 2"})]})}}}]);
+//# sourceMappingURL=138.0e1b9ecd.chunk.js.map
