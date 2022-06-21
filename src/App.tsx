957a5c06bc2f7afa851371de58a2ad70efb264c2
chaelin kim <gjf0055@gmail.com>
2022-06-21 21:28:34 +0900

    프로젝트 이미지 변경경

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import loadable from "@loadable/component";
 import { Route, Routes } from "react-router";
 
-const Portfolio = loadable(() => import("/portfolio"));
+const Portfolio = loadable(() => import("./project/portfolio"));
 const Toy1 = loadable(() => import("./project/toy1"));
 const Toy2 = loadable(() => import("./project/toy2"));
 
@@ -10,7 +10,7 @@ const App = (): JSX.Element => {
   return (
     <Routes>
       <Route path="/" element={<Portfolio />} />
-      <Route path="/page1" element={<Toy1 />} />
+      <Route path="/main" element={<Toy1 />} />
       {/* <Route path="/page2" element={<Toy2 />} /> */}
     </Routes>
   );
